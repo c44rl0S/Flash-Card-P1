16da63efb6474b973636bea16681b57a0474964d
c44rl0S <159548784+c44rl0S@users.noreply.github.com>
2024-09-12 12:01:40 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Karatê',
+    'Qual é o primeiro kata do karatê?',
+    'o primeiro kata do karatê é o Heian Shodan'
+)
+
+criaCartao(
+    'Basquete',
+    'Quantos pontos vale a enterrada?',
+    'A enterrada vale 2 pontos'
+)
+
+criaCartao(
+    'Truco',
+    'Com quantos pontos acaba uma partida de truco?',
+    'A partida acaba quando um jogador ou a dupla chegar a 12 pontos'
+)
+
+criaCartao(
+    'Animais',
+    'Por que a galinha atravessou a rua?',
+    'Pra chegar ao outro lado'
+)
