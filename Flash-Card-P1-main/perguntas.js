c36c321446dd526f7299f66d44fc1176ab08528b
c44rl0S <159548784+c44rl0S@users.noreply.github.com>
2024-09-27 12:20:12 -0300

    Add files via upload

---
+++ Flash-Card-P1-main/perguntas.js
@@ -0,0 +1,179 @@
+criaCartao(
+    'Karatê',
+    'Qual é o primeiro kata do karatê?',
+    'o primeiro kata do karatê é o Heian Shodan'
+)
+
+criaCartao(
+    'Basquete',
+    'Quantos pontos vale a enterrada?',
+    'A enterrada vale 2 pontos'
+)
+
+criaCartao(
+    'Truco',
+    'Com quantos pontos acaba uma partida de truco?',
+    'A partida acaba quando um jogador ou a dupla chegar a 12 pontos'
+)
+
+criaCartao(
+    'Animais',
+    'Por que a galinha atravessou a rua?',
+    'Pra chegar ao outro lado'    
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo?',
+    'Rússia'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o rio mais longo do mundo?',
+    'O Rio Nilo'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o país mais populoso da África?',
+    'Nigéria'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Austrália?',
+    'Canberra'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior deserto o mundo?',
+    'Deserto do Saara'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o primeiro imperador romano?',
+    'Augusto'
+)
+
+criaCartao(
+    'História',
+    'Em que ano ocorreu a Revolução Francesa?',
+    '1789'
+)
+
+criaCartao(
+    'História',
+    'Qual foi o principal líder da independência da Índia?',
+    'Mahatma Gandhi'
+)
+
+criaCartao(
+    'História',
+    'Quem foi a primeira mulher a ganhar um prêmio Nobel?',
+    'Marie Curie'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o primeiro imperador romano?',
+    'Augusto'
+)
+
+criaCartao(
+    'História',
+    'Em que ano ocorreu a Revolução Francesa?',
+    '1789'
+)
+
+criaCartao(
+    'História',
+    'Qual foi o principal líder da independência da Índia?',
+    'Mahatma Gandhi'
+)
+
+criaCartao(
+    'História',
+    'Quem foi a primeira mulher a ganhar um prêmio Nobel?',
+    'Marie Curie'
+)
+
+criaCartao(
+    'História',
+    'Qual foi o evento que iniciou a Segunda Guerra Mundial?',
+    'Invasão da Polônia'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o rio mais longo do mundo?',
+    'Nilo'
+)
+
+criaCartao(
+    'Geografia',
+    'Em que continente está localizado o deserto do Saara?',
+    'África'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Austrália?',
+    'Canberra'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual país tem mais ilhas no mundo?',
+    'Suécia'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde está localizada a Grande Muralha?',
+    'China'
+)
+
+criaCartao(
+    'Ciência',
+    'O que é a fotossíntese?',
+    'Processo de conversão de luz em energia pelas plantas'
+)
+
+criaCartao(
+    'Ciência',
+    'Qual é o elemento químico com o símbolo O?',
+    'Oxigênio'
+)
+
+criaCartao(
+    'Ciência',
+    'O que estuda a genética?',
+    'Herdabilidade e variação nos organismos'
+)
+
+criaCartao(
+    'Ciência',
+    'Qual é a unidade básica da vida?',
+    'Célula'
+)
+
+criaCartao(
+    'Ciência',
+    'O que é um átomo?',
+    'A menor unidade de um elemento químico'
+)
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu "A Moreninha"?',
+    'Joaquim Manuel de Macedo'
+)
+
+criaCartao(
+    'Literatura',
+    'Qual autor é conhecido por suas obras de terror, como "Drácula"?',
+    'Bram Stoker'
+)
