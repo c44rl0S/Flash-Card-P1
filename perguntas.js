c8eea14b0613cdcea9ba677bf39c0dea44a02b40
c44rl0S <159548784+c44rl0S@users.noreply.github.com>
2024-10-04 12:20:15 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -51,3 +51,128 @@ criaCartao(
     'Qual é o maior deserto o mundo?',
     'Deserto do Saara'
 )
+criaCartao(
+    'Karatê',
+    'Qual é o objetivo do karatê?',
+    'O objetivo é a autodefesa e o desenvolvimento pessoal.'
+)
+
+criaCartao(
+    'Karatê',
+    'Qual é a faixa mais alta no karatê?',
+    'A faixa preta.'
+)
+
+criaCartao(
+    'Basquete',
+    'Quantos jogadores há em um time de basquete?',
+    'Cinco jogadores.'
+)
+
+criaCartao(
+    'Basquete',
+    'Qual é o tamanho da quadra de basquete oficial?',
+    '28 metros de comprimento por 15 metros de largura.'
+)
+
+criaCartao(
+    'Truco',
+    'Quantas cartas são distribuídas em uma partida de truco?',
+    'Três cartas por jogador.'
+)
+
+criaCartao(
+    'Truco',
+    'Qual é o valor das cartas de manilha?',
+    'As manilhas têm valores superiores às demais cartas.'
+)
+
+criaCartao(
+    'Animais',
+    'Qual é o animal terrestre mais rápido?',
+    'O guepardo.'
+)
+
+criaCartao(
+    'Animais',
+    'Qual é o maior mamífero do mundo?',
+    'A baleia azul.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Brasil?',
+    'Brasília.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual país é conhecido como a Terra do Sol Nascente?',
+    'Japão.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o menor país do mundo?',
+    'Vaticano.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o continente mais populoso?',
+    'Ásia.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior oceano do mundo?',
+    'Oceano Pacífico.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a montanha mais alta do mundo?',
+    'Monte Everest.'
+)
+
+criaCartao(
+    'Animais',
+    'Qual ave é conhecida por imitar sons?',
+    'O bulbul.'
+)
+
+criaCartao(
+    'Animais',
+    'Qual é o animal símbolo da Austrália?',
+    'O canguru.'
+)
+
+criaCartao(
+    'Truco',
+    'Como é chamado o jogo de truco em algumas regiões do Brasil?',
+    'Truco paulista.'
+)
+
+criaCartao(
+    'Truco',
+    'Quais são as principais cartas do truco?',
+    'As cartas de manilha.'
+)
+
+criaCartao(
+    'Basquete',
+    'Qual é a altura da cesta de basquete?',
+    '3,05 metros.'
+)
+
+criaCartao(
+    'Basquete',
+    'Quantos tempos tem um jogo de basquete profissional?',
+    'Quatro tempos.'
+)
+
+criaCartao(
+    'Karatê',
+    'Qual é o significado da palavra karatê?',
+    'Mão vazia.'
+)
